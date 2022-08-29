a8fe3bb7fad698219c05b6a06e5edd0e66ed32b9
MoizArshad10 <moiz_ahmed5k70@hotmail.com>
2022-08-29 13:23:34 +0500

    final commit

--- api/api/settings.py
+++ api/api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f^k)*tqxbun6z#f2wb7hcaqek+r(+7f1c)8kdb1f%%&+l(44)i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','djangoocrud.herokuapp.com']
 
 
 # Application definition
